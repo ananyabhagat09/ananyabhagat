bd1d3dc2e7ccaa2550f1b07c4a72ee2c11e61683
ananyabhagat09 <ananyabhagat91@gmail.com>
2026-08-13 16:07:05 +0530

    Update LoadingScreen.jsx

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,51 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 import "./LoadingScreen.scss";
 
 import { useProgress } from "@react-three/drei";
 
 import Button from "../Button/Button";
 
-import {
-  playSound,
-  playBackgroundMusic,
-} from "../../utils/audioSystem";
-
+import { playSound, playBackgroundMusic } from "../../utils/audioSystem";
 import { useAudioStore } from "../../Experience/stores/audioStore";
-
-const loadingMessages = [
-  "Placing the blocks...",
-  "Connecting the paths...",
-  "Building the environment...",
-  "Putting the finishing touches in place...",
-];
 
 const LoadingScreen = () => {
   const { progress } = useProgress();
-
   const [isRevealed, setIsRevealed] = useState(false);
   const [isAnimationFinished, setIsAnimationFinished] = useState(false);
-  const [messageIndex, setMessageIndex] = useState(0);
-
   const { setIsAudioEnabled } = useAudioStore();
-
-  useEffect(() => {
-    if (progress >= 100) return;
-
-    const interval = setInterval(() => {
-      setMessageIndex((current) =>
-        current < loadingMessages.length - 1
-          ? current + 1
-          : current
-      );
-    }, 1500);
-
-    return () => clearInterval(interval);
-  }, [progress]);
 
   const handleReveal = () => {
     setIsAudioEnabled(true);
     setIsRevealed(true);
-
     playBackgroundMusic();
     playSound("buttonClick");
   };
@@ -58,69 +30,42 @@ const LoadingScreen = () => {
     return null;
   }
 
-  const loadingComplete = progress >= 100;
-
   return (
     <>
       <div className="loading-screen">
-
         <div
-          className={`background-top-half ${
-            isRevealed ? "revealed" : ""
-          }`}
+          className={`background-top-half ${isRevealed ? "revealed" : ""}`}
           onTransitionEnd={handleAnimationFinished}
         ></div>
-
         <div
-          className={`background-bottom-half ${
-            isRevealed ? "revealed" : ""
-          }`}
+          className={`background-bottom-half ${isRevealed ? "revealed" : ""}`}
         ></div>
-
         <div className="loading-screen-info-container">
-
-          {/* Greeting */}
           <div
             className={`intro-message-container ${
               isRevealed ? "revealed" : ""
             }`}
           >
-            Hi 👋! Thanks for stopping by!
+            Hi👋! Thanks for stopping by!! ❤️
           </div>
-
-          {/* Loading message */}
-          {!loadingComplete && (
-            <div
-              className={`instructions-container ${
-                isRevealed ? "revealed" : ""
-              }`}
-            >
-              {loadingMessages[messageIndex]}
-            </div>
-          )}
-
-          {/* Loading bar */}
-          {!loadingComplete && (
+          <div
+            className={`instructions-container ${isRevealed ? "revealed" : ""}`}
+          >
+            🖱️ Drag/Scroll Up/Down to Navigate~ 👈
+          </div>
+          {progress < 100 ? (
             <div className="loading-bar-container">
               <div
                 className="loading-bar"
                 style={{ width: `${progress}%` }}
               ></div>
+              <div className="percentage">{Math.round(progress)}%</div>
             </div>
-          )}
-
-         {/* Ready state */}
-{loadingComplete && !isRevealed && (
-  <>
-    <div className="ready-scroll-message">
-      Scroll ↓ to explore the world ✦
-    </div>
-
-    <Button onClick={handleReveal}>
-      &nbsp;&nbsp;&nbsp; Enter World &nbsp;&nbsp;&nbsp;
-    </Button>
-  </>
-)}
+          ) : !isRevealed ? (
+            <Button onClick={handleReveal}>
+              &nbsp; &nbsp; &nbsp; Enter World &nbsp; &nbsp; &nbsp;
+            </Button>
+          ) : null}
         </div>
       </div>
     </>
